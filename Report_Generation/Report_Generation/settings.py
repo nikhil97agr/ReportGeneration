fd773d85b9ee99bfd292cf2a07dd3f4d4d6a5862
nikhil97agr <nikhil97agrawal@gmail.com>
2022-08-19 23:42:40 +0530

    changes

--- Report_Generation/Report_Generation/settings.py
+++ Report_Generation/Report_Generation/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'vriddhi',
         'USER': 'root',
-        'PASSWORD': 'Newuser@12345',
+        'PASSWORD': 'root',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }

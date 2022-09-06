7513da5408c8b29fb7b68ee6b63fb61de101ad8e
nikhil97agr <nikhil97agrawal@gmail.com>
2022-09-06 13:03:34 +0530

    changes

--- Report_Generation/dashboard/static/assets/js/acc_state_and_transaction.js
+++ Report_Generation/dashboard/static/assets/js/acc_state_and_transaction.js
@@ -2,6 +2,10 @@
 var mem_prev_all = "";
 var data_fetched = false;
 $(document).ready(() => {
+
+    $("#overlay_close").click(() => {
+        $("#table_overlay").hide();
+    })
     $("#search").click(() => {
         const member_id = $("#member_id").val();
 
@@ -90,8 +94,10 @@ const separate_year_month = (data) => {
 }
 
 const populate_data = (data) => {
+    $("#acc_state_trans").empty();
     $.each(data, (key, value) => {
         const years = []
+        const table_data = {}
         var temp = `<div class="row">
             <div class="col-lg-1 col-md-1"></div>
             <div class="col-lg-11 col-md-11 col-sm-12 col-xs-12">
@@ -115,12 +121,12 @@ const populate_data = (data) => {
                temp+= `<div class="row">
                                     <div class="col-lg-1 col-md-1"></div>
                                     <div class="col-lg-11 col-md-11 col-sm-12 col-xs-12">
-                                        <label>${month}</label>
+                                        <label id="${key}_${year}_${month}">${month}</label>
 
                                     </div>
                                 </div>`
 
-
+                table_data[`${key}_${year}_${month}`] = month_data;
             })
             temp+= `</div>
                         </div>
@@ -140,11 +146,60 @@ const populate_data = (data) => {
         $.each(years, (idx, obj) => {
             register_click_acc(`${key}_${obj}`, `${key}_${obj}_month`);
         })
+
+        $.each(table_data, (key, value) => {
+            populate_table(key, value);
+        })
     })
 
 
 
 
+}
+
+
+const populate_table = (id, data) => {
+    $(`#${id}`).click(() => {
+        $("#table_overlay").show();
+        $("#account_state_overlay").empty();
+        $("#transactions_table_overlay").empty();
+        const acc_state = data[0];
+        const transaction = data[1];
+        $("#account_state_overlay").append(`
+            <tr>
+            <td>${acc_state.id}</td>
+            <td>${acc_state.account_number}</td>
+            <td>${acc_state.date_of_update}</td>
+            <td>${acc_state.balance_ob}</td>
+            <td>${acc_state.due}</td>
+            <td>${acc_state.overdue_ob}</td>
+            <td${acc_state.PanelCharge}</td>
+            <td>${acc_state.TotalCharge}</td>
+            <td>${acc_state.TransferIn_CB}</td>
+            <td>${acc_state.TransferOut_CB}</td>
+            <td>${acc_state.balance_cb}</td>
+            <td>${acc_state.Overdue_CB}</td>
+            </tr>
+        
+        `)
+
+
+        $.each(transaction, (idx, obj) => {
+            $("#transactions_table_overlay").append(
+                `<tr>
+                <td>${obj.id}</td> 
+                <td>${obj.acc_num}</td> 
+                <td>${obj.date}</td> 
+                <td>${obj.amount}</td> 
+                <td>${obj.crdr}</td> 
+                <td>${obj.transaction_type}</td> 
+                <td>${obj.rule}</td> 
+                <td>${obj.trans_type_name}</td> 
+                </tr>`
+            )
+        })
+        console.log(id,transaction)
+    });
 }
 
 
